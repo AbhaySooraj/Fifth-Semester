4388a553b8bcbe0cc1a41935f17020e9d06dbb85
AbhaySooraj <94735651+AbhaySooraj@users.noreply.github.com>
2023-11-14 22:13:44 +0530

    Create preprocessing.R

---
+++ Machine Learning/preprocessing.R
@@ -0,0 +1,20 @@
+library(mlbench)
+library(ggplot2)
+library(caret)
+
+soybean_df <- read.csv("C:\\Users\\abhay\\Desktop\\Main\\RAJAGIRI\\S5\\ML\\Soybean.csv")
+
+
+soybean <- na.omit(soybean_df)
+
+#normalization or scaling
+preproc <- preProcess(soybean[, -1], method = c("center", "scale"))
+soybean[, -1] <- predict(preproc, soybean[, -1])
+set.seed(123)  # For reproducibility
+
+#split 80% to training and 20% to test data
+splitIndex <- createDataPartition(soybean$Class, p = 0.8, list = FALSE)
+training_data <- soybean[splitIndex, ]
+training_data
+testing_data <- soybean[-splitIndex, ]
+testing_data
