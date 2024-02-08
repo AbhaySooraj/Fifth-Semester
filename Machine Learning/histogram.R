047345b516fc5fd9f354d565a1874a0c095dfd08
AbhaySooraj <94735651+AbhaySooraj@users.noreply.github.com>
2024-02-08 21:14:24 +0530

    Update histogram.R

--- Machine Learning/histogram.R
+++ Machine Learning/histogram.R
@@ -1,18 +1,11 @@
-setwd(file.path("C:\\Users\\student\\Desktop\\abhay\\Exp3"))
-# Load the necessary packages
 library(ggplot2)
 
+titanic_df <- read.csv("C:\\Users\\abhay\\Desktop\\Main\\RAJAGIRI\\S5\\ML\\titanic.csv")
 
-
-# Convert the dataset to a data frame
-titanic_df <- read.csv("titanic.csv")
-
-# Create a histogram of passenger ages
 ggplot(titanic_df, aes(x = Age)) +
   geom_histogram(binwidth = 5, fill = "black", color = "white") +
   labs(
     title = "Passenger Age Distribution on the Titanic",
     x = "Age",
     y = "Count"
-  ) +
-  
+  ) 
