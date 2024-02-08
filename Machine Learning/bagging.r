615ec971a97b6ed212af39f019b31cc7a3e457cd
AbhaySooraj <94735651+AbhaySooraj@users.noreply.github.com>
2024-02-08 23:25:51 +0530

    Update bagging.r

--- Machine Learning/bagging.r
+++ Machine Learning/bagging.r
@@ -1,20 +1,13 @@
-#Bagging
-install.packages("ipred")
-
 library(ipred)
 library(caret)
 
-data(iris)
-head(iris)
-
 set.seed(123)
 
-bagged_model<-bagging(Species ~ ., data = iris, nbagg = 50)
-bagged_model
+bagged_model<- bagging(Species ~ ., data = iris, nbagg=50)
 
-predictions<-predict(bagged_model,newdata = iris)
-predictions
+predictions<-predict(bagged_model,iris)
 
-table(predictions,iris$Species)
+iris$Species<-as.factor(iris$Species)
+predictions<-as.factor(predictions)
 
 confusionMatrix(predictions,iris$Species)
